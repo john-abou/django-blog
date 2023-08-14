7cc0cd5a8b5b4021152b5293e85a230d2a6ea2c7
john-abou <abounassar.john@gmail.com>
2023-08-14 16:49:28 -0400

    Home template -- not dynamic yet

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'), # this route is for the home page
     path('posts', views.posts, name='posts'),
     path('posts/<slug:slug>', views.post_detail, name='post'), #posts/this-is-a-slug-post
 ]
